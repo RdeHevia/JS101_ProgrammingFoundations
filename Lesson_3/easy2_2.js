7585e5b8c610832ff570c102bce1c2de5db79f9b
Raul de Hevia <raul.dehevia@gmail.com>
2020-03-21 11:34:45 -0700

    version 1

--- Lesson_3/easy2_2.js
+++ Lesson_3/easy2_2.js
@@ -7,5 +7,11 @@ console.log(reversedNumbers);
 
 
 numbers = [1, 2, 3, 4, 5];
-numbers.sort((num1, num2) => num2 - num1);
+let reversedNumbers2 = [...numbers].sort((num1, num2) => num2 - num1);
 console.log(numbers); // [ 5, 4, 3, 2, 1 ]
+console.log(reversedNumbers2);
+
+let reversedNumbers3 = numbers.slice().sort((num1, num2) => num2 - num1);
+console.log(numbers); // [ 5, 4, 3, 2, 1 ]
+console.log(reversedNumbers3);
+
