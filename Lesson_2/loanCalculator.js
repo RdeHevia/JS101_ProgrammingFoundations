254a0f4ff6a57af2714cf10baaf165764f134409
Raul de Hevia <raul.dehevia@gmail.com>
2020-04-19 10:45:48 -0700

    Version 2: Input validation improved, functions to print output added

--- Lesson_2/loanCalculator.js
+++ Lesson_2/loanCalculator.js
@@ -9,10 +9,13 @@ function emptyLines (nbrOfLines) {
 }
 
 function getFloatInput() {
-  let nbr = parseFloat(rlsync.question());
-  while (Number.isNaN(nbr) || nbr<0) {
-    prompt('Number equal or greater than 0 expected. Please enter a valid value.');
-    nbr = parseFloat(rlsync.question());
+  let nbrString = rlsync.question();
+  let nbr = parseFloat(nbrString);
+  while (Number.isNaN(nbr) || nbr < 0 || nbrString.includes(','))  {
+    prompt('Number equal or greater than 0 expected. Comma not all allowed.');
+    prompt('Please enter a valid value.')
+    nbrString = rlsync.question();
+    nbr = parseFloat(nbrString);
   }
   return nbr;
 }
@@ -21,8 +24,25 @@ function loanToMonth(presentValue,interestMonth,nMonths) {
   if (interestMonth === 0) {
     return presentValue / nMonths;
   } else {
-    return presentValue * (interestMonth / (1 - (1 + interestMonth) ** (-nMonths)));
+    return (
+      presentValue * (interestMonth / (1 - ((1 + interestMonth) ** (-nMonths))))
+    );
   }
+}
+
+function printInputSummary(loan, apr, nbrMonths) {
+  emptyLines(3);
+  console.log(`SUMMARY:`);
+  prompt(`Loan: $ ${loan}`);
+  prompt(`APR: ${apr} %`);
+  prompt(`Loan duration: ${nbrMonths} months`);
+}
+
+function printResultsSummary (monthlyPay, interestPaid, totalPay) {
+  emptyLines(1);
+  prompt(`Monthly payment: $ ${monthlyPay}`);
+  prompt(`Total interests to be paid: $ ${interestPaid}`);
+  prompt(`Total amount to be paid: $ ${totalPay}`);
 }
 
 emptyLines(3);
@@ -33,10 +53,10 @@ prompt('Enter the loan amount.');
 let loan = getFloatInput();
 
 prompt('Enter the Annual Percent Rate. APR (%).');
-let apr = parseFloat(rlsync.question());
+let apr = getFloatInput();
 
 prompt('Enter the loan duration in months.');
-let nbrMonths = parseFloat(rlsync.question());
+let nbrMonths = getFloatInput();
 
 let intMonth = apr / 12 / 100;
 
@@ -44,17 +64,8 @@ let monthlyPay = loanToMonth(loan,intMonth,nbrMonths).toFixed(2);
 let totalPay = (monthlyPay * nbrMonths).toFixed(2);
 let interestPaid = (totalPay - loan).toFixed(2);
 
-emptyLines(3);
-console.log(`SUMMARY:`);
-prompt(`Loan: $ ${loan}`);
-prompt(`APR: ${apr} %`);
-prompt(`Loan duration: ${nbrMonths} months`);
-
-emptyLines(1);
-prompt(`Monthly payment: $ ${monthlyPay}`);
-prompt(`Total interests to be paid: $ ${interestPaid}`);
-prompt(`Total amount to be paid: $ ${totalPay}`);
-
+printInputSummary(loan, apr, nbrMonths);
+printResultsSummary (monthlyPay, interestPaid, totalPay);
 emptyLines(3);
 
 
